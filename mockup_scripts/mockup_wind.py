44d7b4d470a247dca87bd17b9929328a0e45d277
Chanok Pathompatai <cpathompatai@gmail.com>
2026-03-23 17:11:39 +0700

    remove .env

--- mockup_scripts/mockup_wind.py
+++ mockup_scripts/mockup_wind.py
@@ -3,7 +3,7 @@ import time
 import random
 import json
 
-MQTT_BROKER = "172.27.228.246"
+MQTT_BROKER = "192.168.1.37"
 MQTT_PORT = 1883
 MQTT_TOPIC = "wind_data"
 
--- mockup_scripts/mockup_wind_direction.py
+++ mockup_scripts/mockup_wind_direction.py
@@ -3,7 +3,7 @@ import time
 import random
 import json
 
-MQTT_BROKER = "192.168.10.100"
+MQTT_BROKER = "192.168.1.37"
 MQTT_PORT = 1883
 MQTT_TOPIC = "winds_direction"
 
